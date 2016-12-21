0e5ce1d261d5d71ad14ccba191343070745c4037
Andrew Lavers <andrew@smooch.io>
2016-12-21 10:03:16 -0500

    Initial commit

---
+++ convert.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+const data = fs.readFileSync('spellData.json');
+const json = JSON.parse(data);
+const spells = {};
+json.forEach((spell) => {
+    spells[spell.name] = spell;
+});
+
+fs.writeFileSync('out.json', JSON.stringify(spells, null, 2));
